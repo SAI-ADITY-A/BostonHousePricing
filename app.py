abdc6a817df51c2275bf2a37d657f135f4ab46de
Sai Aditya <21mm01022@iitbbs.ac.in>
2024-06-07 20:13:14 +0530

    Commited

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 # Load the model
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
+
 
 
 @app.route('/')
@@ -19,4 +21,12 @@ def home():
 def predict_api():
     data = request.json['data']
     print(data)
-    
+    print(np.array(list(data.values())))
+    new_data = scaler.transform(np.array(list(data.values())))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
